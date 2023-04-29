f128991ac93404ffbe1a0b765f3ba8406f68687b
Leandro Evangelista Melo <leandro.melo@yahoo.com.br>
2023-04-29 01:48:32 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  Titulo = 'Projeto - eita dana... show';
+}
